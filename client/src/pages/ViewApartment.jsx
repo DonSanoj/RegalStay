ce1d7df07e77723c4dae22d76ca5e727d1d6f76c
DonSanoj <sanojaminda@gmail.com>
2025-08-02 16:21:33 +0530

    Add 3D model

--- client/src/pages/ViewApartment.jsx
+++ client/src/pages/ViewApartment.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import { useRef, useState } from "react"
+import { Canvas, useFrame } from "@react-three/fiber"
+import { SoftShadows, Float, CameraControls, Sky, PerformanceMonitor } from "@react-three/drei"
+import { Perf } from "r3f-perf"
+import { easing } from "maath"
+import { Model as Room } from "../components/includes/Room"
 
-const ViewApartment = () => {
+function Light() {
+    const ref = useRef()
+    useFrame((state, delta) => {
+        easing.dampE(ref.current.rotation, [(state.pointer.y * Math.PI) / 50, (state.pointer.x * Math.PI) / 20, 0], 0.2, delta)
+    })
     return (
-        <div>ViewApartment</div>
+        <group ref={ref}>
+            <directionalLight position={[5, 5, -8]} castShadow intensity={5} shadow-mapSize={2048} shadow-bias={-0.001}>
+                <orthographicCamera attach="shadow-camera" args={[-8.5, 8.5, 8.5, -8.5, 0.1, 20]} />
+            </directionalLight>
+        </group>
     )
 }
 
-export default ViewApartment
+export default function App() {
+    const [bad, set] = useState(false)
+    
+    const debug = false;
+    const enabled = true;
+    const config = { size: 35, focus: 0.5 };
+    const samples = 16;
+
+    return (
+        <Canvas shadows camera={{ position: [5, 2, 10], fov: 50 }}>
+            {debug && <Perf position="top-left" />}
+            <PerformanceMonitor onDecline={() => set(true)} />
+            {enabled && <SoftShadows {...config} samples={bad ? Math.min(6, samples) : samples} />}
+            <CameraControls makeDefault />
+            <color attach="background" args={["#d0d0d0"]} />
+            <fog attach="fog" args={["#d0d0d0", 8, 35]} />
+            <ambientLight intensity={0.4} />
+            <Light />
+            <Room scale={0.5} position={[0, -1, 0]} />
+            <Sphere />
+            <Sphere position={[2, 4, -8]} scale={0.9} />
+            <Sphere position={[-2, 2, -8]} scale={0.8} />
+            <Sky inclination={0.52} scale={20} />
+        </Canvas>
+    )
+}
+
+function Sphere({ color = "hotpink", floatIntensity = 15, position = [0, 5, -8], scale = 1 }) {
+    return (
+        <Float floatIntensity={floatIntensity}>
+            <mesh castShadow position={position} scale={scale}>
+                <sphereGeometry />
+                <meshBasicMaterial color={color} roughness={1} />
+            </mesh>
+        </Float>
+    )
+}
